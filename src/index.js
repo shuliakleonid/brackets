cc026c4fd945e370895b064e5a76c9008064d9ae
Leonid Shuliak <zeleny777@gmail.com>
2020-10-04 16:10:07 +0300

    innit: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let checkPoint = 1;
+    while(checkPoint == 1) {
+        let pastStr = str;
+        bracketsConfig.map(function (a){
+        str = str.replace(a[0] + a[1], '')
+      }
+      )
+        if (pastStr === str)  { 
+          checkPoint = 0; 
+        }
+    }
+    return (str.length == 0) ? true : false;
 }
